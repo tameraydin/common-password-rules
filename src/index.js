6641022bb50aec96aeca104110aacddba41cd54c
Tamer Aydin <tamer.aydin@gmail.com>
2016-02-25 00:36:47 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+'use strict'
+
+module.exports = {
+  containsLowerCase: (password, times) => {
+    return;
+  },
+  containsUpperCase: (password, times) => {
+    return;
+  },
+  containsNumber: (password, times) => {
+    return;
+  },
+  containsSpecialChar: (password, times) => {
+    return;
+  },
+  excludesRecurringChars: (password, times) => {
+    return;
+  },
+  excludesConsecutiveLetters: (password, times) => {
+    return;
+  },
+  excludesConsecutiveNumbers: (password, times) => {
+    return;
+  },
+  excludesPossibleBirthYear: (password) => {
+    return;
+  }
+};
---
+++ test/test.js
@@ -0,0 +1,8 @@
+import test from 'ava';
+import commonPasswordRules from '../src/index';
+
+const cPR = commonPasswordRules;
+
+test('containsNumberOf()', t => {
+  // t.is(cPR.containsNumber(), 1);
+});
