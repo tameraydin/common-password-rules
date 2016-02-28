b8fe68c445a8cc16ac67a979b52812dd6553e771
Tamer Aydin <tamer.aydin@gmail.com>
2016-02-28 01:50:26 +0100

    implemented methods

--- index.es2015.js
+++ index.es2015.js
@@ -1,28 +1,85 @@
 'use strict'
 
+const ALPHABET_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+const ALPHABET_UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const NUMBERS = '0123456789';
+
+const defaultsTo = function(val, def) {
+  return typeof val === 'number' ? val : def;
+};
+
+const regTest = function(exp, str) {
+  return new RegExp(exp, 'g').test(str);
+};
+
+const containsSequentialsFrom = function(orderedLists, password, times) {
+  times = defaultsTo(times, 3);
+  let len = password.length;
+  let passwordArr = password.split('');
+  let found = false;
+
+  password.split('').forEach((letter, index) => {
+    if (found || len - times + 1 <= 0) {
+      return;
+    }
+
+    let foundIndex = Math.max(orderedLists[0].indexOf(letter),
+      orderedLists[1] ? orderedLists[1].indexOf(letter) : -1);
+
+    if (foundIndex > -1) {
+      for (let i = 1; i < times; i++) {
+        if (orderedLists[0][foundIndex + i] !== password.charAt(index + i) &&
+            (!orderedLists[1] ||
+            orderedLists[1][foundIndex + i] !== password.charAt(index + i))) {
+          return;
+        }
+      }
+
+      found = true;
+    }
+  });
+
+  return found;
+};
+
 module.exports = {
   containsLowerCase: (password, times) => {
-    return;
+    return regTest('(.*?[a-z]){' + defaultsTo(times, 1) + ',}',
+      password);
   },
   containsUpperCase: (password, times) => {
-    return;
+    return regTest('(.*?[A-Z]){' + defaultsTo(times, 1) + ',}',
+      password);
   },
-  containsNumber: (password, times) => {
-    return;
+  containsDigit: (password, times) => {
+    return regTest('(.*?\\d){' + defaultsTo(times, 1) + ',}',
+      password);
   },
   containsSpecialChar: (password, times) => {
-    return;
+    return regTest('(.*?[_\\W]){' + defaultsTo(times, 1) + ',}',
+      password);
+  },
+  contains: (password, charSet, times) => {
+    return regTest('(.*?' + charSet + '){' + defaultsTo(times, 1) + ',}',
+      password);
   },
   excludesRecurringChars: (password, times) => {
-    return;
+    return !regTest('(\\w)\\1{' + (defaultsTo(times, 3) - 1) + ',}',
+      password);
   },
-  excludesConsecutiveLetters: (password, times) => {
-    return;
+  excludesSequentialLetters: (password, times) => {
+    return !containsSequentialsFrom(
+      [ALPHABET_LOWERCASE, ALPHABET_UPPERCASE], password, times);
   },
-  excludesConsecutiveNumbers: (password, times) => {
-    return;
+  excludesSequentialNumbers: (password, times) => {
+    return !containsSequentialsFrom(
+      [NUMBERS], password, times);
   },
-  excludesPossibleBirthYear: (password) => {
-    return;
+  excludesPossibleBirthDate: (password) => {
+    return !regTest(
+        '([0123]\\d|[a-zA-Z_/.-])' +
+        '([0123]\\d|[a-zA-Z_/.-])' +
+        '(19[6789]\\d|20[0]\\d)',
+      password);
   }
 };
--- test.js
+++ test.js
@@ -3,6 +3,80 @@ import commonPasswordRules from './index.es2015';
 
 const cPR = commonPasswordRules;
 
-test('containsNumberOf()', t => {
-  // t.is(cPR.containsNumber(), 1);
+test('containsLowerCase()', t => {
+  t.is(cPR.containsLowerCase('FOO'), false);
+  t.is(cPR.containsLowerCase('Bar'), true);
+
+  t.is(cPR.containsLowerCase('FOo', 2), false);
+  t.is(cPR.containsLowerCase('bAr', 2), true);
+});
+
+test('containsUpperCase()', t => {
+  t.is(cPR.containsUpperCase('foo'), false);
+  t.is(cPR.containsUpperCase('Bar'), true);
+
+  t.is(cPR.containsUpperCase('Foo', 2), false);
+  t.is(cPR.containsUpperCase('BaR', 2), true);
+});
+
+test('containsDigit()', t => {
+  t.is(cPR.containsDigit('foo'), false);
+  t.is(cPR.containsDigit('bar1'), true);
+
+  t.is(cPR.containsDigit('foo1', 2), false);
+  t.is(cPR.containsDigit('b1a1r', 2), true);
+});
+
+test('containsSpecialChar()', t => {
+  t.is(cPR.containsSpecialChar('foo'), false);
+  t.is(cPR.containsSpecialChar('bar*'), true);
+
+  t.is(cPR.containsSpecialChar('foo{', 2), false);
+  t.is(cPR.containsSpecialChar('b&a#r', 2), true);
+  t.is(cPR.containsSpecialChar('+bar_', 2), true);
+});
+
+test('contains()', t => {
+  let charSet = '[$\\\\]';
+
+  t.is(cPR.contains('foo', charSet), false);
+  t.is(cPR.contains('b$r', charSet), true);
+  t.is(cPR.contains('ba\\', charSet), true);
+
+  t.is(cPR.contains('foo\\', charSet, 2), false);
+  t.is(cPR.contains('\\b$r', charSet, 2), true);
+  t.is(cPR.contains('b$$r', charSet, 2), true);
+});
+
+test('excludesRecurringChars()', t => {
+  t.is(cPR.excludesRecurringChars('fooo'), false);
+  t.is(cPR.excludesRecurringChars('baar'), true);
+  t.is(cPR.excludesRecurringChars('f000o'), false);
+  t.is(cPR.excludesRecurringChars('0bar00'), true);
+
+  t.is(cPR.excludesRecurringChars('f__', 2), false);
+  t.is(cPR.excludesRecurringChars('_bar_', 2), true);
+});
+
+test('excludesSequentialLetters()', t => {
+  t.is(cPR.excludesSequentialLetters('abc123'), false);
+  t.is(cPR.excludesSequentialLetters('xabyz'), true);
+
+  t.is(cPR.excludesSequentialLetters('xCdy', 2), false);
+  t.is(cPR.excludesSequentialLetters('AxBy', 2), true);
+});
+
+test('excludesSequentialNumbers()', t => {
+  t.is(cPR.excludesSequentialNumbers('abc789'), false);
+  t.is(cPR.excludesSequentialNumbers('x901yz'), true);
+
+  t.is(cPR.excludesSequentialNumbers('x01y', 2), false);
+  t.is(cPR.excludesSequentialNumbers('A1B2', 2), true);
+});
+
+test('excludesPossibleBirthDate()', t => {
+  t.is(cPR.excludesPossibleBirthDate('foo1990'), false);
+  t.is(cPR.excludesPossibleBirthDate('01011960'), false);
+  t.is(cPR.excludesPossibleBirthDate('199bar'), true);
+  t.is(cPR.excludesPossibleBirthDate('3012200'), true);
 });
